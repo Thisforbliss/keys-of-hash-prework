f25a4b293e67d390e13eebe6917d4b88762c24d2
Malcolm Leake <echelon1089@gmail.com>
2018-08-30 00:13:05 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,9 +3,10 @@ class Hash
   def keys_of(*arguments)
  
     self.each do |animal, country|
+     binding.pry
       if country == arguments
         return animal.to_a
-   binding.pry
+ 
       end
     end
   end
